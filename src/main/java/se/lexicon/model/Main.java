53fea75d23ad0e9239b8aa866d9ad3ae097c32e9
AgnesNazie <cheagnesmbo@gmail.com>
2025-04-07 15:19:52 +0200

    updated TestTodoItemTask class

--- src/main/java/se/lexicon/model/Main.java
+++ src/main/java/se/lexicon/model/Main.java
@@ -3,6 +3,5 @@ package se.lexicon.model;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-        TestTodoItemTask.main(args);
     }
 }
